536852a531826f2f45efb967d4beecc389403aea
Jon Jagger <jon@jaggersoft.com>
2019-07-24 10:56:21 +0100

    add script to bring up cron container using docker-compose command

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -3,6 +3,9 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+echo
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
-  build
+  up \
+  -d \
+  --force-recreate

8aa683e9a21445b59e98c372cdd01f66695493b8
JonJagger <jon@jaggersoft.com>
2019-07-29 10:16:12 +0100

    ensure SHA is set in build-script in its own right

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,11 +2,21 @@
 set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
-docker-compose \
-  --file "${ROOT_DIR}/docker-compose.yml" \
-  build
+build_service_image()
+{
+  echo
+  docker-compose \
+    --file "${ROOT_DIR}/docker-compose.yml" \
+      build \
+        "${1}"
+}
 
-readonly TMP_SHA=$(docker run --rm cyberdojo/cron sh -c 'echo -n ${SHA}')
-readonly TAG=${TMP_SHA:0:7}
-docker tag cyberdojo/cron:latest cyberdojo/cron:${TAG}
+build_service_image cron
+
+# Assuming we do not have any new puller commits, puller's latest commit
+# sha will match the image tag inside versioner's .env file.
+# This means we can tag to it and a [cyber-dojo up] call
+# will use the tagged image.
+docker tag cyberdojo/cron:latest cyberdojo/cron:${SHA:0:7}
